1bab0c3a17df05a81308221e54d7123ef27e475f
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2025-07-17 10:15:35 +0300

    Add project

---
+++ 3. Introduction to Hibernate/Lab/IntroductionToHibernate/src/main/java/bg/softuni/Main.java
@@ -0,0 +1,36 @@
+package bg.softuni;
+
+import bg.softuni.entities.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.SQLOutput;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Configuration config = new Configuration();
+        config.configure();
+
+        SessionFactory sessionFactory = config.buildSessionFactory();
+        Session currentSession = sessionFactory.openSession();
+
+        currentSession.getTransaction().begin();
+
+        /*
+        Student student = new Student();
+        student.setName("Pesho");
+
+        currentSession.persist(student);
+        */
+
+        Student found = currentSession.find(Student.class, 1);
+        System.out.println(found.getName());
+
+        currentSession.getTransaction().commit();
+
+        currentSession.close();
+    }
+}
---
+++ 3. Introduction to Hibernate/Lab/IntroductionToHibernate/src/main/java/bg/softuni/entities/Student.java
@@ -0,0 +1,29 @@
+package bg.softuni.entities;
+
+public class Student
+{
+    private long id;
+    private String name;
+
+    public Student() {}
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
